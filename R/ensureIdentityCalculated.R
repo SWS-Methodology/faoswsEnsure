25774ea6736b0830cd46445836006d61eeb5ffef
mk <mkao006@gmail.com>
2016-06-10 16:14:30 +0200

    corrected denormalisation

--- R/ensureIdentityCalculated.R
+++ R/ensureIdentityCalculated.R
@@ -7,6 +7,8 @@
 ##' @param prodVar The column name corresponding to produciton.
 ##' @param returnData logical, whether the data should be returned.
 ##' @param normalised logical, whether the data is normalised
+##' @param denormalisedKey optional, only required if the input data is not
+##'     normalised.It is the name of the key that denormalises the data.
 ##' @param getInvalidData logical, this will skip the test and extract the data
 ##'     that is invalid.
 ##' @return If getInvalidData is FALSE, then the data is returned when the test
@@ -23,12 +25,13 @@ ensureIdentityCalculated = function(data,
                                     prodVar,
                                     returnData = TRUE,
                                     normalised = TRUE,
+                                    denormalisedKey = "measuredElement",
                                     getInvalidData = FALSE){
 
     dataCopy = copy(data)
 
     if(normalised){
-        dataCopy = denormalise(dataCopy, "measuredElement")
+        dataCopy = denormalise(dataCopy, denormalisedKey)
     }
 
     ensureDataInput(data = dataCopy,
@@ -55,14 +58,13 @@ ensureIdentityCalculated = function(data,
     invalidData = dataCopy[identityNotCalculated, ]
 
     if(getInvalidData){
-        if(!normalised){
-            invalidData = denormalise(invalidData, denormalisedKey)
+        if(normalised){
+            invalidData = normalise(invalidData)
         }
         return(invalidData)
     } else {
 
         if(nrow(invalidData) > 0){
-            print(identityNotCalculated)
             stop("Not all entries are calculated")
         }
 
